d5d7dd3feeb0a0fb8b21c64f70519de7c98df289
rzm0572 <1271920576@qq.com>
2020-01-27 16:59:06 +0800

    Fix some bugs

--- main.py
+++ main.py
@@ -5,21 +5,23 @@
 from tiebaSpider import TiebaSpider
 
 code = input("请输入帖子编号：")
-add_info_inp = input("是否添加信息(y/[n])：")
 only_lz_inp = input("是否只看楼主([y]/n)：")
-path = input("请输入存储位置：")
 
-add_info = False
 only_lz = True
-
-if add_info_inp is not None and add_info_inp == 'y':
-    add_info = True
-
 if only_lz_inp is not None and only_lz_inp == 'n':
     only_lz = False
 
 try:
-    contents = TiebaSpider(code, path, add_info, only_lz)
+    ts = TiebaSpider(code, only_lz)
+
+    add_info = False
+    if ts.page_type == 1:
+        add_info_inp = input("是否添加信息(y/[n])：")
+        if add_info_inp is not None and add_info_inp == 'y':
+            add_info = True
+
+    path = input("请输入存储位置：")
+    ts.get_contents(code, path, add_info, only_lz)
 except Exception:
     print("ERROR!")
 finally:
--- tiebaSpider.py
+++ tiebaSpider.py
@@ -8,16 +8,37 @@ from bs4 import BeautifulSoup
 
 
 class TiebaSpider(object):
-    HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
-                Chrome/55.0.2883.87 Safari/537.36'}
+    HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/79.0.3945.130 Safari/537.36'}
     total_page = -1
     page_title = ""
     lz = ""
     contents = ""
+    page_type = 0
 
-    def __init__(self, number, path, add_info=False, only_lz=True):
+    def __init__(self, number, only_lz=True):
         self.get_page_info(number, only_lz)
-        self.get_contents(number, path, add_info, only_lz)
+
+    def parse_page_info(self, first_page_soup):
+        total_page_div = first_page_soup.find('ul', class_='l_posts_num')
+        page_title_tag1 = first_page_soup.find('h3', class_=re.compile('core_title_txt pull-left text-overflow'))
+        page_title_tag2 = first_page_soup.find('h1', class_=re.compile('core_title_txt'))
+        self.lz = first_page_soup.find('a', class_=re.compile('p_author_name')).contents[0].string
+        total_page_string = total_page_div.find('li', class_=re.compile('l_reply_num')).contents[2].string
+        if total_page_string.isdigit():
+            self.total_page = int(total_page_string)
+        else:
+            self.total_page = -1
+
+        if page_title_tag1 is not None and page_title_tag2 is None:
+            self.page_type = 1
+            self.page_title = page_title_tag1.string
+        if page_title_tag1 is None and page_title_tag2 is not None:
+            self.page_type = 0
+            self.page_title = page_title_tag2.string
+        if page_title_tag1 is None and page_title_tag2 is None:
+            raise Exception("Page error!")
+        # print(self.page_type)
 
     def get_page_info(self, number, only_lz=True):
         url = "https://tieba.baidu.com/p/%s?see_lz=%d" % (str(number), only_lz)
@@ -28,15 +49,22 @@ class TiebaSpider(object):
             raise Exception("Connection error!")
         first_page_html = first_page.text
         first_page_soup = BeautifulSoup(first_page_html, 'lxml')
-        total_page_div = first_page_soup.find('ul', class_='l_posts_num')
-        self.page_title = first_page_soup.find('h3', class_='core_title_txt pull-left text-overflow').string
-        self.lz = first_page_soup.find('a', class_=re.compile('p_author_name')).contents[0].string
-        total_page_string = total_page_div.contents[3].contents[2].string
-        if total_page_string.isdigit():
-            self.total_page = int(total_page_string)
-        else:
-            self.total_page = -1
+
+        self.parse_page_info(first_page_soup)
+
         return [self.total_page, self.page_title, self.lz]
+
+    def get_post_info(self, page_soup, add_info, only_lz):
+        info = []
+        cz = []
+        if add_info:
+            if self.page_type == 1:
+                info = page_soup.find_all('div', class_=re.compile('post-tail-wrap'))
+            elif self.page_type == 0:
+                info = page_soup.find_all('div', class_='core_reply_tail')
+            if not only_lz:
+                cz = page_soup.find_all('a', class_=re.compile('p_author_name.*j_user_card'))
+        return [info, cz]
 
     def get_contents(self, number, path, add_info=False, only_lz=True):
         if self.total_page == -1:
@@ -57,13 +85,11 @@ class TiebaSpider(object):
             print("Connection OK!")
             page_html = page.text
             page_soup = BeautifulSoup(page_html, 'lxml')
-            posts = page_soup.find_all('div', class_='d_post_content j_d_post_content')
-            info = []
-            cz = []
-            if add_info:
-                info = page_soup.find_all('div', class_='post-tail-wrap')
-                if not only_lz:
-                    cz = page_soup.find_all('a', class_=re.compile('p_author_name'))
+            posts = page_soup.find_all('div', class_=re.compile('d_post_content j_d_post_content'))
+
+            post_info = self.get_post_info(page_soup, add_info, only_lz)
+            info = post_info[0]
+            cz = post_info[1]
 
             for post in posts:
                 if add_info:
@@ -73,7 +99,7 @@ class TiebaSpider(object):
                                                                               '(0?[1-9]|[12][0-9]|3[01]) ([01][0-9]'
                                                                               '|2[0-3]):([0-5][0-9])')).string
                     if not only_lz:
-                        self.contents += ' by ' + cz[idx].contents[0].string
+                        self.contents += ' by ' + cz[idx].get_text()
                     self.contents += '\n'
                 self.contents += post.get_text('\n', 'br/') + '\n\n'
                 self.render(file)
